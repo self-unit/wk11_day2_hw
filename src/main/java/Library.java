7f8f30aeaa5b9a6b4551bad71be69edb0ec16983
self-unit <self_unit@hotmail.com>
2018-10-09 16:44:47 +0100

    Book hasGenre test working

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> stock;
+
+    public Library() {
+        this.stock = new ArrayList<>();
+    }
+
+    public int getBookCount() {
+        return stock.size();
+    }
+
+    public boolean addBook(Book book) {
+        return stock.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    Book book1;
+    Book book2;
+    Book book3;
+    Book book4;
+    Library library;
+
+    @Before
+    public void before() {
+        book1 = new Book("Neuromancer", "William Gibson", "Sci-Fi");
+        book2 = new Book("Life of Pi", "Yann Martel", "Fiction");
+        book3 = new Book("Candide", "Voltaire", "Fiction");
+        book4 = new Book("Democracy in America", "Alexis de Tocqueville", "Non-Fiction");
+        library = new Library();
+    }
+
+    @Test
+    public void addBook() {
+        assertEquals(true, library.addBook(book1));
+    }
+
+    @Test
+    public void getBookCount() {
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        assertEquals(4, library.getBookCount());
+    }
 }
