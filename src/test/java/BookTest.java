553a0284810fe49dcbaed87d66ace96c6bfaa0b5
self-unit <self_unit@hotmail.com>
2018-10-09 16:05:55 +0100

    first commit

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+    Book book;
+
+    @Before
+    public void before() {
+        book = new Book();
+    }
+
+    @Test
+    public void hasName() {
+
+    }
+}
