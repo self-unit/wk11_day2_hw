1a70df481f3051f10d0ed7a8ce3f6614193a66d5
self-unit <self_unit@hotmail.com>
2018-10-09 16:23:33 +0100

    Book hasTitle test working

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,16 +1,18 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
     Book book;
 
     @Before
     public void before() {
-        book = new Book();
+        book = new Book("Neuromancer", "William Gibson", "Sci-Fi");
     }
 
     @Test
-    public void hasName() {
-
+    public void hasTitle() {
+        assertEquals("Neuromancer", book.getTitle());
     }
 }
